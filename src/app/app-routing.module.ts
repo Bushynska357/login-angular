89ab6af6f4c05b345a3987305429dca08ac4741d
Bushynska357 <77163973+Bushynska357@users.noreply.github.com>
2021-04-30 10:17:36 +0300

    change incorrect paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './modules/authorization/components/login/login.component';
-import { TodoComponent } from './modules/main/components/todo/todo.component';
+
 
 const routes: Routes = [
-  { path: 'login', loadChildren: './modules/authorization/authorization.module#AuthorizationModule'},
-  { path: 'todo', loadChildren: './modules/main/main.module#MainModule'},
+  { path: 'login', loadChildren: () => import('./modules/authorization/authorization.module').then(m => m.AuthorizationModule)},
+  { path: 'todo', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
